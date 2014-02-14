cac29505b82b55e83e0dc43579af64aa03a03ac1
joebos <frank_lqx@yahoo.com>
2014-02-13 20:10:40 -0500

    custom modification

--- examples/example1/app.js
+++ examples/example1/app.js
@@ -13,7 +13,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3002);
+  app.set('port', process.env.PORT || 6032);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -42,6 +42,63 @@ restfulMongo = new RestfulMongo({
 }) 
 
 restfulMongo.configure(app, config );
+
+/*var TwitterSearch = require('./lib/twittersearch.js')
+var twitter = new TwitterSearch()
+twitter.setTWSearchGet(app);*/
+
+
+var exec = require('child_process').exec;
+
+function setTwitterSearchGet (app) {
+    var self=this
+    /**
+     * Query
+     */
+    console.log('TwitterSearch', 'Configuring GET')
+
+    app.get('/api/twittersearch/?', function(req, res) {
+
+        var keyword = req.query.keyword
+
+        var userid = req.query.username
+        var leadcat = req.query.leadcat
+
+
+        var execStr = 'phantomjs /home/white/charlesbank/leads/get-tweeters.js "' +  userid + '" 1 "' + keyword + '" 1 1 1 1';
+
+        console.log(execStr);
+
+        child = exec(execStr, function callback(error, stdout, stderr) {
+
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+                res.json(500, {"result": "failure", "error": error } );
+            }
+
+            res.json(200, {"result": "success"});
+            /*var output = stdout.trim();
+
+             // In my casperJS script I echo "TIME OUT" if my casper js tests timed out due to connectivity issues, bad performance, etc.
+
+             if (output.indexOf("TIME OUT") === -1) {
+
+             // NO TIME OUT so casperjs returned results and lets process them!
+
+             var results = JSON.parse(output);
+
+             // Results now contains an expected JSON KVO, array, etc.
+
+             // Process results using normal nodejs packages/libraries
+             }*/
+        });
+
+    });
+}
+
+setTwitterSearchGet(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
