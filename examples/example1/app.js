32e4ae0e00bb40a649234ea2a39c6b895b95da4a
joebos <frank_lqx@yahoo.com>
2016-02-23 15:31:55 +0000

    ...

--- examples/example1/app.js
+++ examples/example1/app.js
@@ -20,7 +20,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
+  app.use(express.bodyParser({limit: '5mb'}));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
